2d60c249ede64b5d67cc4b22542adb2579d3be5c
Devinix00 <dpldpl123456@naver.com>
2024-02-27 23:40:57 +0900

    [CREATE] router 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/home/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
+
 function App() {
-  return <></>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
