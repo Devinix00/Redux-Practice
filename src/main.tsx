ad2810e0871ba930dcd5c22c9697c580f5786a0c
Devinix00 <dpldpl123456@naver.com>
2024-02-28 14:22:40 +0900

    [CREATE] combineReducers 적용

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { createStore } from "redux";
-import counter from "./redux/reducers/counter.ts";
 import { Provider } from "react-redux";
+import rootReducer from "./redux/reducers/index.ts";
 
-const store = createStore(counter);
+const store = createStore(rootReducer);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
---
+++ src/redux/index.d.ts
@@ -0,0 +1,3 @@
+interface IRootState {
+  counter: number;
+}
